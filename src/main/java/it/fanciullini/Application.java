bb499fb75418263f6b0fcd82c9e7d5661e47f3ea
Luca Fanciullini <luca.fanciullini@gmail.com>
2018-04-15 21:08:08 +0200

    Added bootstrap

--- src/main/java/it/fanciullini/Application.java
+++ src/main/java/it/fanciullini/Application.java
@@ -2,9 +2,6 @@ package it.fanciullini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
